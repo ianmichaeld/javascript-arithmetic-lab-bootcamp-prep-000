31c201c60e5e69550ce5e007f217d9d57f210e75
Ian Dewsbury <ianmichaeld@gmail.com>
2018-01-05 21:43:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ function multiply(a,b){return a*b;}
 
 function divide(a,b){return a/b;}
 
-function inc(n){return n++;}
+function inc(n){return (n++);}
 
-function dec(n){return n--;}
+function dec(n){return (n--);}
 
 function makeInt(n){return parseInt(n);}
